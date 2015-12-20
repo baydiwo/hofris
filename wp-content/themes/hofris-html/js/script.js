17cb77be5553ce2c1891992d81d3fb84dd43b3b6
Bayu Adi Wibowo <baydiwo@gmail.com>
2015-12-20 08:30:36 +0700

    -Custom Date

--- wp-content/themes/hofris-html/js/script.js
+++ wp-content/themes/hofris-html/js/script.js
@@ -34,12 +34,4 @@ $(document).ready(function(){
         side: 'right',
         // onOpen : function
     });
-
-    $("input").on("change", function() {
-        this.setAttribute(
-            "data-date",
-            moment(this.value, "YYYY-MM-DD")
-            .format( this.getAttribute("data-date-format") )
-        )
-    }).trigger("change")
 });
